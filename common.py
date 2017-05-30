4ff9bf037d4fee60895560177ace3099c7410c36
Bas Nijholt <basnijholt@gmail.com>
2017-05-30 15:31:21 +0200

    Implement funcs.run_simulation and use named_product

--- common.py
+++ common.py
@@ -1,12 +1,51 @@
 """Common functions for doing stuff."""
 
+from datetime import timedelta
 import functools
 from glob import glob
 from itertools import product
+import os
 import subprocess
 
 import numpy as np
 import pandas as pd
+from toolz import partition_all
+
+
+def run_simulation(lview, func, vals, parameters, fname_i, N=None,
+                   overwrite=False):
+    if N is None:
+        N = 1000000
+        if len(vals) > N:
+            raise Exception('You need to split up vals in smaller parts')
+
+    N_files = len(vals) // N + (0 if len(vals) % N == 0 else 1)
+    print('`vals` will be split in {} files.'.format(N_files))
+    time_elapsed = 0
+    parts_done = 0
+    for i, chunk in enumerate(partition_all(N, vals)):
+        fname = fname_i.replace('{}', '{:03d}').format(i)
+        print('Busy with file: {}.'.format(fname))
+        if not os.path.exists(fname) or overwrite:
+            map_async = lview.map_async(func, chunk)
+            map_async.wait_interactive()
+            result = map_async.result()
+            df = pd.DataFrame(result)
+            df = df.assign(**parameters)
+            df = df.assign(git_hash=get_git_revision_hash())
+            df.to_hdf(fname, 'all_data', mode='w', complib='zlib', complevel=9)
+
+            # Print useful information
+            N_files_left = N_files - (i + 1)
+            parts_done += 1
+            time_elapsed += map_async.elapsed
+            time_left = timedelta(seconds=(time_elapsed / parts_done) *
+                                  N_files_left)
+            print_str = ('Saved {}, {} more files to go, {} time to go '
+                         'to complete everything.')
+            print(print_str.format(fname, N_files_left, time_left))
+        else:
+            print('File: {} was already done.'.format(fname))
 
 
 def parse_params(params):
